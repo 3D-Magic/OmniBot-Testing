6dc280ebe0fdd4787fd63eabcb6fb81bbddc5643
3D Magic <155168455+3D-Magic@users.noreply.github.com>
2026-04-14 16:05:04 +1200

    Delete config directory

--- config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-OMNIBOT Configuration Module
-Handles settings persistence and constants
-"""
-
-from .settings import Settings
-from .constants import *
-
-__all__ = ['Settings', 'SETTINGS_FILE', 'DEFAULT_SETTINGS']
