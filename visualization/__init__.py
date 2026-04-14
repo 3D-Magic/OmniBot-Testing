e441a8c74bac73620e68c2c08749c6285ab0f317
3D Magic <155168455+3D-Magic@users.noreply.github.com>
2026-04-14 16:05:56 +1200

    Delete visualization directory

--- visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-OMNIBOT Visualization Module
-Handles dashboard routes and graph plotting
-"""
-
-from .dashboard_routes import dashboard_bp, init_dashboard
-from .graph_plotter import GraphPlotter
-
-__all__ = ['dashboard_bp', 'init_dashboard', 'GraphPlotter']
