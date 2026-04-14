ff9efa870696e0fea308df47cef2509795291411
3D Magic <155168455+3D-Magic@users.noreply.github.com>
2026-04-14 16:05:16 +1200

    Delete engine directory

--- engine/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-OMNIBOT Trading Engine Module
-Handles trading logic and execution
-"""
-
-from .trading_engine import TradingEngine, OrderSide, OrderType, TradingStrategy
-
-__all__ = ['TradingEngine', 'OrderSide', 'OrderType', 'TradingStrategy']
